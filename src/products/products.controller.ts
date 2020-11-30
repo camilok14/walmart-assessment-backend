79cac2c9d987d330b11e374a2d48531c8da3a5aa
Camilo Salazar <camilok14@gmail.com>
2020-11-29 22:29:16 -0300

    implements products controller in order to satisfy its proposed unit tests

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { FindProductsInputDto } from './dto/find-products-input.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsSercvice: ProductsService) {}
+
+  @Get('')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  public async findProducts(@Query() productsQuery: FindProductsInputDto): Promise<Product[]> {
+    const { searchString } = productsQuery;
+    return await this.productsSercvice.findProductsBySearchString(searchString);
+  }
+}
