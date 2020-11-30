6558a84d31a6691bf354ad17bf31900dac289af6
Camilo Salazar <camilok14@gmail.com>
2020-11-29 22:51:48 -0300

    implements products service in order to satisfy proposed unit test

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product, ProductDocument } from './schemas/product.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(@InjectModel(Product.name) private readonly productModel: Model<ProductDocument>) {}
+
+  public async findProductsBySearchString(searchString: string): Promise<Product[]> {
+    const onlyNumbers = /^[0-9]+$/.test(searchString);
+    const moreThan3Chars = searchString.length >= 3;
+    if (!onlyNumbers && !moreThan3Chars) {
+      throw new HttpException('Search strings shorter than 3 characters must be valid product id', HttpStatus.BAD_REQUEST);
+    }
+    const $or = [];
+    if (onlyNumbers) {
+      $or.push({ id: parseInt(searchString) });
+    }
+    if (moreThan3Chars) {
+      const $regex = `.*${searchString}.*`;
+      $or.push({ description: { $regex } });
+      $or.push({ brand: { $regex } });
+    }
+    return this.productModel.find({ $or });
+  }
+}
