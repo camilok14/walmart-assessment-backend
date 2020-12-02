c845a90e5eff045f82234dd8e066b9d607cb75c0
Camilo Salazar <camilok14@gmail.com>
2020-12-02 00:14:05 -0300

    fix origin cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['https://walmart-assessment-frontend.herokuapp.com/', 'http://localhost:8080'] });
+  app.enableCors({ origin: ['https://walmart-assessment-frontend.herokuapp.com', 'http://localhost:8080'] });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
