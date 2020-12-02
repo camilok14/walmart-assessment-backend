33f7263dfa14d2c566d743bf4f364188d6207faf
Camilo Salazar <camilok14@gmail.com>
2020-12-02 12:27:41 -0300

    includes localhost with no port on cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['https://walmart-assessment-frontend.herokuapp.com', /http:\/\/localhost:[0-9]+$/] });
+  app.enableCors({ origin: ['https://walmart-assessment-frontend.herokuapp.com', 'http://localhost', /http:\/\/localhost:[0-9]+$/] });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
